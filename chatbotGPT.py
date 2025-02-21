420ea850749cf6c87868c5d6a29a8521843c5a97
rainy <RainyWANG@runoob.com>
2025-02-21 22:59:13 +0800

    25_02_21 commit

---
+++ chatbotGPT.py
@@ -0,0 +1,44 @@
+import configparser
+import requests
+import logging
+from ChatGPT_HKBU import HKBU_ChatGPT
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+def equiped_chatgpt(update, context):
+    global chatgpt
+    reply_message = chatgpt.submit(update.message.text)
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+
+def main():
+    # Load your token and create an Updater
+    config = configparser.ConfigParser()
+    config.read('./configGAI.ini')
+    updater = Updater(token=config['TELEGRAM']['ACCESS_TOKEN'], use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Register a dispatcher to handle message: here we register an echo dispatcher
+    # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # dispatcher.add_handler(echo_handler)
+
+    # Dispatcher for chatgpt
+    global chatgpt
+    chatgpt = HKBU_ChatGPT(config)
+    chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
+    dispatcher.add_handler(chatgpt_handler)
+
+    # Start the Bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
